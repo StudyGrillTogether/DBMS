55afc1e5fc67e5c8af31d4f2bd54ee5d86450d44
Kshitij1ghk <kshitijwk@gmail.com>
2025-09-14 11:33:43 +0530

    DAY 3- query for adding student's data

--- Insert_StudentData.sql
+++ Insert_StudentData.sql
@@ -1,2 +1,11 @@
 INSERT INTO student(id,name,age,course,enrollment_date)
-VALUE();
+VALUES
+(1,'Asterion',31,'ComputerScience','2024-09-05'),
+(2,'Anvil',35,'ComputerScience','2024-09-05'),
+(3,'Kisong',23,'Biology','2024-09-05'),
+(4,'Neph',16,'History','2024-09-05'),
+(5,'Sunny',16,'History','2024-09-05'),
+(6,'Cassie',17,'Chemistry','2024-09-05'),
+(7,'Nightangle',29,'Music','2024-09-05'),
+(8,'Auro',46,'ComputerScience','2024-09-05'),
+(9,'Effie',30,'Physics','2024-09-05');
